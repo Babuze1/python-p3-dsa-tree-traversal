b65994e7393b1473cb134cc36c1c8e3a58fed2cf
Babuze <134912091+Babuze1@users.noreply.github.com>
2023-09-06 21:06:22 +0300

    Completed assignment

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,32 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, node_data):
+        self.root = node_data
 
-  def get_element_by_id(self, id):
-    pass
+    def depth_first_search(self, node, target_id):
+        if node['id'] == target_id:
+            return node
+
+        for child in node['children']:
+            result = self.depth_first_search(child, target_id)
+            if result:
+                return result
+
+        return None
+
+    def breadth_first_search(self, target_id):
+        queue = [self.root]
+
+        while queue:
+            node = queue.pop(0)
+            if node['id'] == target_id:
+                return node
+
+            queue.extend(node['children'])
+
+        return None
+
+    def get_element_by_id(self, target_id, depth_first=True):
+        if depth_first:
+            return self.depth_first_search(self.root, target_id)
+        else:
+            return self.breadth_first_search(target_id)
